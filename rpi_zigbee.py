186801a5358e850535293a892ce089380a9004bb
LeeJeho <leejen2003@naver.com>
2018-04-29 19:58:44 +0900

    add '.' value detect

--- rpi_zigbee.py
+++ rpi_zigbee.py
@@ -8,6 +8,8 @@ xbee.write('Hello from Raspberry Pi')
 
 type = 1
 data = 0
+under = 0
+data_under = 0
 data_type = 0
 data_value = 0
 
@@ -18,18 +20,29 @@ try:
             if ('0' <= i <= '9') & (type == 1) :
                 data_type = data_type*10 + int(i)
             if ('0' <= i <= '9') & (data == 1) :
-                data_value = data_value*10 + int(i)
+                if(under == 1):
+                    data_under = data_under*10 + int(i)
+                else:
+                    data_value = data_value*10 + int(i)
             if i == ',':
                 type = 0
                 data = 1
+            if i == '.':
+                under = 1
+                data_value = str(data_value) + '.'
             if i == ';':
                 type = 1
                 data = 0
+                under = 0
+                if data_under != 0:
+                    data_value += str(data_under)
                 print("data_type : " + str(data_type))
                 print("data_value : " + str(data_value))
                 data_type = 0
                 data_value = 0
+                data_under = 0
 
 except KeyboardInterrupt:
     xbee.write('Bye from Raspberry Pi')
     xbee.close()
+
